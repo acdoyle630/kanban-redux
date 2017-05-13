fc0c9007e2cd3de54988b18eb63a2ceab47917f1
Adam <acdoyle630@gmail.com>
2017-05-13 10:11:47 -1000

    initial set up

---
+++ src/lib/cards.db.js
@@ -0,0 +1,27 @@
+/*jshint esversion: 6*/
+
+const CardsFromFakeDB = [{
+    _id : 1,
+    title : 'Get a new tire',
+    priority : 'High',
+    status : 'Queue',
+    createdBy : 'Adam',
+    assignedTo : 'Adam'
+  },
+  {
+   id : 2,
+    title : 'Laundry',
+    priority : 'Medium',
+    status : 'Queue',
+    createdBy : 'Adam',
+    assignedTo : 'Kat'
+  },
+  {
+   id : 3,
+    title : 'Dishes',
+    priority : 'Low',
+    status : 'Queue',
+    createdBy : 'Adam',
+    assignedTo : 'Matt'
+  }
+];
