7fd56d3aaafcfa1a6dbea0815ac27f295dea190f
Adam <acdoyle630@gmail.com>
2017-05-16 18:40:19 -1000

    hashed passwords

--- api/users/index.js
+++ api/users/index.js
@@ -1,16 +1,28 @@
 /*jshint esversion: 6*/
 
+const path = require('path');
 const express = require('express');
 const users = express.Router();
 const { User } = require('../../models');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const passportLocal = require('passport-local');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 users.post('/',(req, res) =>{
-
-  console.log(req.body);
-  User.create(req.body);
-  // .then(card => {
-  // //   res.json( card );
-  // });
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+      User.create({
+        username : req.body.username,
+        password : hash
+      })
+      .then((user) =>{
+        console.log(user);
+      });
+    });
+  });
 });
 
 
